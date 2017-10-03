1b90ddc74762b1c87d1d15125874da86cc8577da
Peter Beattie <peterbeattie19@gmail.com>
2017-10-03 13:36:37 +0100

    Add files via upload

---
+++ Luhns.java
@@ -0,0 +1,32 @@
+public class Luhns {
+    public static void main(String[] args) {
+        System.out.print(checkDigit("7992739871"));
+    }
+
+    public static int checkDigit(String card){
+    	int sum = 0;
+    	int alter = 0;
+
+    		for(int i = card.length(); i>=1; i--){
+    			int temp = 0;
+
+    			if(alter==0){
+    				temp = Integer.parseInt(card.substring(i-1,i));
+    					if(temp*2>9) sum += (temp*2 - 9);
+
+    						else sum+= temp*2;
+
+    				alter = 1;
+
+    			}
+
+    			else {
+    				sum += Integer.parseInt(card.substring(i-1,i));
+    				alter = 0;
+    			}
+    		//	System.out.println(sum);
+    		}
+
+    		return (sum*9)%10;
+    }
+}
