39041aab67013065a74718455949b90ce7f19d4e
Peter Beattie <peterbeattie19@gmail.com>
2017-12-03 16:24:27 +0000

    Update and rename Lab9.java to Lab8.java

--- Lab8.java
+++ Lab8.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Lab9 {
+public class Lab8 {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     Queue pq = new Queue();
