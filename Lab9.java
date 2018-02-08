fb18170545ae04ead4d2ff844bfecb0507e0f7cc
Peter Beattie <peterbeattie19@gmail.com>
2018-02-08 22:17:30 +0000

    Create Lab9.java

---
+++ Lab9.java
@@ -0,0 +1,23 @@
+
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
+A Node is defined as: 
+    class Node {
+        int data;
+        Node next;
+    }
+*/
+
+boolean hasCycle(Node head) {
+    if(head==null || head.next==null) return false;
+    
+    HashSet<Node> set = new HashSet<>(); 
+    
+    while(head != null){
+        if(set.add(head) == false)
+            return true;
+        head = head.next; 
+    }
+    
+    return false;
+}
